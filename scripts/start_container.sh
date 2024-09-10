4252af1d74e46f67e911b8256a7197a89f193613
seena127 <45530763+seena127@users.noreply.github.com>
2024-09-10 15:19:48 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,4 +1,3 @@
 set -e
 docker pull bsreenu1999/py-app
-img_id= docker images | awk -F'\t' '$2 ~ /latest/ {print $2}'
-docker run -d -p 5000:80 $img_id --name sampl-py-app
+docker run -d -p 5000:80 bsreenu1999/py-app
