3a3e87da960a8cd367d332f8ae29fe74866a7525
seena127 <45530763+seena127@users.noreply.github.com>
2024-09-10 19:55:55 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker push bsreenu1999/py-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 bsreenu1999/py-app:latest
